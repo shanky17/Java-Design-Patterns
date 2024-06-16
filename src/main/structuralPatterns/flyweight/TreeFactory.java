47667610077a73f0e87c649627b01b488663b568
shanky17 <shashaank.soni@gmail.com>
2024-06-16 18:22:44 +0530

    Added flyweight design pattern

---
+++ src/main/structuralPatterns/flyweight/TreeFactory.java
@@ -0,0 +1,14 @@
+package main.structuralPatterns.flyweight;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TreeFactory {
+    static Map<String, TreeType> treeTypes = new HashMap<>();
+
+    public static TreeType getTreeType(String name, Color color, String otherTreeData) {
+
+        return treeTypes.computeIfAbsent(name, n -> new TreeType(n, color, otherTreeData));
+    }
+}
